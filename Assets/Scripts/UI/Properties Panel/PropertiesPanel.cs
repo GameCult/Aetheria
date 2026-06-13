87b622441e591c1f0be4b74e7063f954ad0fee93
Meta Vens <rwvens@hotmail.com>
2026-06-13 12:29:32 +0100

    Use typed runtime catalog for item manufacturer UI

--- Assets/Scripts/UI/Properties Panel/PropertiesPanel.cs
+++ Assets/Scripts/UI/Properties Panel/PropertiesPanel.cs
@@ -390,7 +390,7 @@ public class PropertiesPanel : MonoBehaviour
 			AddProperty("Quantity", () => simpleCommodity.Quantity.ToString());
 		
 		var sheet = AddStatSheet();
-		var manufacturer = GameManager.ItemManager.GetCatalogEntry<Faction>(data.Manufacturer);
+		var manufacturer = ActionGameManager.RuntimeCatalog?.FindCorporationByLegacyId(data.Manufacturer.ToString());
 		sheet.AddStat("Manufacturer", () => manufacturer?.Name ?? "GameCult");
 		sheet.AddStat("Mass", () => ActionGameManager.PlayerSettings.Format(GameManager.ItemManager.GetMass(item)));
 		
